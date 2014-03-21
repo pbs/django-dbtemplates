47e5c306d069e62a9fb6070057261e598a86ca3d
Zoltan Szeredi <zoltan.szeredi@3pillarglobal.com>
2014-03-21 14:45:59 +0200

    LUN-1373: Change dbtemplates caching mechanism.

--- dbtemplates/loader.py
+++ dbtemplates/loader.py
@@ -3,8 +3,11 @@ from django.db import router
 from django.template import TemplateDoesNotExist
 
 from dbtemplates.models import Template
-from dbtemplates.utils.cache import (cache, get_cache_key,
-                                     set_and_return, get_cache_notfound_key)
+from dbtemplates.utils.cache import (
+    get_cache_key,
+    add_template_to_cache,
+    cache,
+)
 from django.template.loader import BaseLoader
 
 
@@ -18,51 +21,40 @@ class Loader(BaseLoader):
     and ``sites`` with the current site.
     """
     is_usable = True
+    display_format = 'dbtemplates:{origin}:{template_name}:{domain}'
+
+    def make_display_name(self, origin, template_name, domain):
+        return self.display_format.format(
+            origin=origin,
+            template_name=template_name,
+            domain=domain
+        )
 
     def load_and_store_template(self, template_name, cache_key, site, **params):
-        templates = Template.objects.filter(name__exact=template_name, **params).distinct()
+        templates = Template.objects.filter(
+            name__exact=template_name, **params).distinct()
         if not templates:
             raise Template.DoesNotExist(template_name)
         #template names are unique => there should always be a single template returned
         template = templates[0]
         db = router.db_for_read(Template, instance=template)
-        display_name = 'dbtemplates:%s:%s:%s' % (db, template_name, site.domain)
-        return set_and_return(cache_key, template.content, display_name)
+        display_name = self.make_display_name(db, template_name, site.domain)
+        data = add_template_to_cache(template)
+        content = data.get('content')
+        return content, display_name
 
     def load_template_source(self, template_name, template_dirs=None):
-        # The logic should work like this:
-        # * Try to find the template in the cache. If found, return it.
-        # * Now check the cache if a lookup for the given template
-        #   has failed lately and hand over control to the next template
-        #   loader waiting in line.
-        # * If this still did not fail we first try to find a site-specific
-        #   template in the database.
-        # * On a failure from our last attempt we try to load the global
-        #   template from the database.
-        # * If all of the above steps have failed we generate a new key
-        #   in the cache indicating that queries failed, with the current
-        #   timestamp.
         site = Site.objects.get_current()
         cache_key = get_cache_key(template_name)
         if cache:
             try:
-                backend_template = cache.get(cache_key)
-                if backend_template:
-                    return backend_template, template_name
-            except:
+                value = cache.get(cache_key)
+                if value and site.pk in value.get('sites'):
+                    backend_template = value.get('content')
+                    display_name = self.make_display_name('cache', template_name, site.domain)
+                    return backend_template, display_name
+            except:             # XXX : Evil !!!
                 pass
-
-        # Not found in cache, move on.
-        cache_notfound_key = get_cache_notfound_key(template_name)
-        if cache:
-            try:
-                notfound = cache.get(cache_notfound_key)
-                if notfound:
-                    raise TemplateDoesNotExist(template_name)
-            except:
-                raise TemplateDoesNotExist(template_name)
-
-        # Not marked as not-found, move on...
 
         try:
             return self.load_and_store_template(template_name, cache_key,
@@ -74,6 +66,4 @@ class Loader(BaseLoader):
             except (Template.MultipleObjectsReturned, Template.DoesNotExist):
                 pass
 
-        # Mark as not-found in cache.
-        cache.set(cache_notfound_key, '1')
         raise TemplateDoesNotExist(template_name)
--- dbtemplates/utils/cache.py
+++ dbtemplates/utils/cache.py
@@ -10,28 +10,23 @@ def get_cache_backend():
     return get_cache(settings.DBTEMPLATES_CACHE_BACKEND)
 
 cache = get_cache_backend()
+key_format = 'dbtemplates::{template_name}'
 
 
-def get_cache_key(name, site_pk=None):
-    if site_pk is None:
-        site_pk = Site.objects.get_current().pk
-    return 'dbtemplates::%s::%s' % (slugify(name), site_pk)
+def get_cache_key(template_name):
+    return key_format.format(
+        template_name=slugify(template_name)
+    )
 
 
 def get_cache_notfound_key(name, site_pk=None):
     return get_cache_key(name, site_pk) + '::notfound'
 
 
-def remove_notfound_key(instance):
-    # Remove notfound key as soon as we save the template.
-    cache.delete(get_cache_notfound_key(instance.name))
-
-
-def set_and_return(cache_key, content, display_name):
-    # Save in cache backend explicitly if manually deleted or invalidated
+def set_and_return(key, value):
     if cache:
-        cache.set(cache_key, content)
-    return (content, display_name)
+        cache.set(key, value)
+    return value
 
 
 def add_template_to_cache(instance, **kwargs):
@@ -40,31 +35,12 @@ def add_template_to_cache(instance, **kwargs):
     in the database was added or changed.
     """
     remove_cached_template(instance)
-    remove_notfound_key(instance)
-    cache.set(get_cache_key(instance.name), instance.content)
-
-
-def invalidate_cache_for_sites(sender, instance, action, reverse,
-                               model, pk_set, **kwargs):
-    if action != 'post_add':
-        return
-    if isinstance(instance, Site):
-        # model is dbtemplates.models.Template
-        site = instance
-        for template in model.objects.all():
-            if template.pk in pk_set:
-                cache.delete(get_cache_notfound_key(template.name, site.pk))
-            else:
-                cache.delete(get_cache_key(template.name, site.pk))
-    else:
-        # instance is of type dbtempaltes.models.Template
-        # model is django.contrib.sites.models.Site
-        template = instance
-        for site in model.objects.all():
-            if site.pk in pk_set:
-                cache.delete(get_cache_notfound_key(template.name, site.pk))
-            else:
-                cache.delete(get_cache_key(template.name, site.pk))
+    key = get_cache_key(instance.name)
+    value = {
+        'content': instance.content,
+        'sites':  set(instance.sites.values_list('pk', flat=True))
+    }
+    return set_and_return(key, value)
 
 
 def remove_cached_template(instance, **kwargs):
